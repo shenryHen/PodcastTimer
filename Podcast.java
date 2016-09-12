eac520ec457a7ae0a3445908617f8f1e7e511802
Henry Shen <henry.shen@outlook.com>
2016-09-12 17:36:27 -0400

    Deleted comments

--- Podcast.java
+++ Podcast.java
@@ -3,21 +3,15 @@ import java.util.*;
 public class Podcast{
 
 	private String podcastTitle = "";
-	//private Map<String, Podcaster> cast = new HashMap<String, Podcaster>();
 	private List<Podcaster> castMembers = new ArrayList<Podcaster>();
 	Podcaster currentTalking;
 	int currentIndex = -1;
-	//private List<String> podcasterNames = new ArrayList<String>();
-	
+
 	public Podcast(String podcastTitle){
 		this.podcastTitle = podcastTitle;
 	}
 
 	public Podcast(String podcastTitle, String... people ){
-		/*for (String person : people) {
-			cast.put(person, new Podcaster(person));
-			podcasterNames.add(person);
-		}*/
 		for (String person : people ) {
 			castMembers.add(new Podcaster(person));
 		}
@@ -28,15 +22,6 @@ public class Podcast{
 			castMembers.add(new Podcaster(person));
 		}
 	}
-	/*public void startTimer(String person){
-		System.out.println("starting talk counter for " + person);
-
-		currentTalking = cast.get(person);
-		currentTalking.startTalkTimer();
-		
-	}*/
-
-	/*add thrown exceptions here*/
 
 	public void startTimer(int index) throws IndexOutOfBoundsException {
 
@@ -48,17 +33,8 @@ public class Podcast{
 		}
 	}
 
-	/*public void stopTimer(String person){
-		System.out.println("stopping talk counter for " + person);
-
-		if (person.length() != 0){ // NOT very first time staring podcast counter
-			currentTalking.stopTalkTimer();
-			System.out.println(person + "'s current talk time is " + currentTalking.getTalkTimeSec());
-		}
-	}*/
-
 	public void stopTimer(){
-		//System.out.println("current index is " + currentIndex);
+		
 		if(currentTalking != null){
 			currentTalking.stopTalkTimer();
 			System.out.println("stopping timer for " + currentTalking.getName() + "   " + currentTalking.getTalkTimeSec());
@@ -77,6 +53,10 @@ public class Podcast{
 		return castMembers.size();
 	}
 
+	public String getCastMemberName(int index){
+		return castMembers.get(index).getName();
+	}
+
 	@Override
 	public String toString(){
 		StringJoiner joiner = new StringJoiner(", ");
@@ -87,20 +67,4 @@ public class Podcast{
 
 		return joiner.toString();
 	}
-
-	public String getCastMemberName(int index){
-		return castMembers.get(index).getName();
-	}
-
-
-	/*TODO: Implement toString() method here!!!!*/
-/*
-	public long getTalkTime(String name){
-		if (name.length() != 0){
-			return cast.get(name).getTalkTimeSec();
-		}
-		return 0;
-	}
-*/
-	
 }
