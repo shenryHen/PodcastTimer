3a800a99139fe476619ffdc57ebee4b4e7fbc982
Henry Shen <henry.shen@outlook.com>
2016-09-12 17:30:43 -0400

    Added more functionality
    Changed construtors, modified some method names to more clear, removed
    hashmaps in Podcast class to an arraylist

--- Podcast.java
+++ Podcast.java
@@ -1,84 +1,106 @@
 import java.util.*;
-import java.lang.StringBuilder;
 
 public class Podcast{
 
 	private String podcastTitle = "";
-	Map<String, Podcaster> cast = new HashMap<String, Podcaster>();
+	//private Map<String, Podcaster> cast = new HashMap<String, Podcaster>();
+	private List<Podcaster> castMembers = new ArrayList<Podcaster>();
 	Podcaster currentTalking;
-	private List<String> podcasterNames = new ArrayList<String>();
+	int currentIndex = -1;
+	//private List<String> podcasterNames = new ArrayList<String>();
 	
 	public Podcast(String podcastTitle){
 		this.podcastTitle = podcastTitle;
 	}
 
 	public Podcast(String podcastTitle, String... people ){
-		for (String person : people) {
+		/*for (String person : people) {
 			cast.put(person, new Podcaster(person));
 			podcasterNames.add(person);
+		}*/
+		for (String person : people ) {
+			castMembers.add(new Podcaster(person));
 		}
-
-		//castMembers = cast.keys();
 	}
-	public void startTimer(String person){
+
+	public Podcast(String podcastTitle, List<String> people){
+		for (String person : people) {
+			castMembers.add(new Podcaster(person));
+		}
+	}
+	/*public void startTimer(String person){
 		System.out.println("starting talk counter for " + person);
 
 		currentTalking = cast.get(person);
 		currentTalking.startTalkTimer();
 		
+	}*/
+
+	/*add thrown exceptions here*/
+
+	public void startTimer(int index) throws IndexOutOfBoundsException {
+
+		if (currentIndex != index ){
+			currentTalking = castMembers.get(index);
+			currentTalking.startTalkTimer();
+			System.out.println("starting timer for " + currentTalking.getName());
+			currentIndex = index;
+		}
 	}
 
-	public void stopTimer(String person){
+	/*public void stopTimer(String person){
 		System.out.println("stopping talk counter for " + person);
 
 		if (person.length() != 0){ // NOT very first time staring podcast counter
 			currentTalking.stopTalkTimer();
 			System.out.println(person + "'s current talk time is " + currentTalking.getTalkTimeSec());
 		}
-	}
+	}*/
 
-	public String getAllPodcasterNames(){
-		//StringBuilder returnedNames = new StringBuilder();
-		StringJoiner joiner = new StringJoiner(", ");
-		for (String name : podcasterNames) {
-			joiner.add(name);
-			
+	public void stopTimer(){
+		//System.out.println("current index is " + currentIndex);
+		if(currentTalking != null){
+			currentTalking.stopTalkTimer();
+			System.out.println("stopping timer for " + currentTalking.getName() + "   " + currentTalking.getTalkTimeSec());
 		}
-		return joiner.toString();
 	}
 
-	public String getAllPodcasterData(){
+	public String[] getAllPodcasterNames(){
+		String[] names = new String[castMembers.size()];
+		for (int i=0; i<names.length; i++) {
+			names[i]=castMembers.get(i).getName();
+		}
+		return names;
+	}
+
+	public int getCastSize(){
+		return castMembers.size();
+	}
+
+	@Override
+	public String toString(){
 		StringJoiner joiner = new StringJoiner(", ");
-		for (Map.Entry<String, Podcaster> entry : cast.entrySet())
+		for (Podcaster caster : castMembers)
 		{
-		    joiner.add(entry.getKey() + " " + entry.getValue().getTalkTimeSec());
+		    joiner.add(caster.getName()+ ": " + caster.getTalkTimeSec());
 		}
 
 		return joiner.toString();
-
 	}
 
+	public String getCastMemberName(int index){
+		return castMembers.get(index).getName();
+	}
+
+
+	/*TODO: Implement toString() method here!!!!*/
+/*
 	public long getTalkTime(String name){
 		if (name.length() != 0){
 			return cast.get(name).getTalkTimeSec();
 		}
 		return 0;
 	}
-
-	/*@Override
-	public String toString(){
-		StringBuilder names = new StringBuilder();
-		for (Podcaster caster : cast) {
-			names.append(" " + caster.getName() + ',');
-		}
-		return (podcastTitle + " \nPeople in podcast: " + names);
-	}*/
+*/
 	
 }
-
-/*******
-Issues: 
-	- how to mock a radio button? with hash table and making a "currTalker" object
-	- how to start one persons timer and end the previous one without iterating through the table
-
-*******/
--- Podcaster.java
+++ Podcaster.java
@@ -40,4 +40,5 @@ public class Podcaster{
 		else 
 			return false;
 	}
+
 }
